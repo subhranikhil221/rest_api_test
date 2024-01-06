d7152f0760f103649aab9db9af2f04f2aa090efd
subrajit pandey <69635255+subhranikhil221@users.noreply.github.com>
2024-01-06 16:44:34 +0530

    Implemented Public Rest Api

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'post.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter API Integration',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<List<Post>> posts;
+
+  @override
+  void initState() {
+    super.initState();
+    posts = fetchPosts();
+  }
+
+  Future<List<Post>> fetchPosts() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      List<Post> posts = data.map((json) => Post.fromJson(json)).toList();
+      return posts;
+    } else {
+      throw Exception('Failed to load posts');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter JSON-API Integration'),
+      ),
+      body: Center(
+        child: FutureBuilder<List<Post>>(
+          future: posts,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(snapshot.data![index].title),
+                    subtitle: Text(snapshot.data![index].body),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
